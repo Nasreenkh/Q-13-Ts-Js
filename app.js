102385c4989b91e8096c16a6985610a22b2e50db
Nasreen khan <160172100+Nasreenkh@users.noreply.github.com>
2024-05-16 00:39:35 +0500

    This is 45 Question Assignment Q13.
    This is 45 Question Assignment Q13.

---
+++ app.js
@@ -0,0 +1,5 @@
+//Excersice 13
+var transportation = ['Civic', 'Bus', 'Honda', 'Mazda', 'Scotty'];
+for (var i = 0; i < transportation.length; i++) {
+    console.log("I would like to own a " + transportation[i]);
+}
